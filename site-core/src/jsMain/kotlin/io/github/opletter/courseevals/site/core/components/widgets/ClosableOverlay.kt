025f845354c66cf86d181b6ce26ed7e713453389
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-04-16 01:45:24 -0400

    Nit: use modifier fun directly

--- site-core/src/jsMain/kotlin/io/github/opletter/courseevals/site/core/components/widgets/ClosableOverlay.kt
+++ site-core/src/jsMain/kotlin/io/github/opletter/courseevals/site/core/components/widgets/ClosableOverlay.kt
@@ -3,12 +3,8 @@ package io.github.opletter.courseevals.site.core.components.widgets
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.css.CSSTransition
 import com.varabyte.kobweb.compose.css.TransitionTimingFunction
-import com.varabyte.kobweb.compose.ui.attrsModifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
-import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
-import com.varabyte.kobweb.compose.ui.modifiers.onClick
-import com.varabyte.kobweb.compose.ui.modifiers.transition
-import com.varabyte.kobweb.compose.ui.modifiers.zIndex
+import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.overlay.Overlay
 import org.jetbrains.compose.web.css.s
 
@@ -22,9 +18,7 @@ fun ClosableOverlay(open: Boolean, onClose: () -> Unit, onStart: () -> Unit = {}
                 .transition(CSSTransition("background-color", 0.3.s, TransitionTimingFunction.Linear))
                 .zIndex(150)
                 .onClick { onClose() }
-                .attrsModifier {
-                    addEventListener("touchmove") { onClose() }
-                }
+                .onTouchMove { onClose() }
         ) {
             content()
             // has to be inside overlay because of deferred render logic

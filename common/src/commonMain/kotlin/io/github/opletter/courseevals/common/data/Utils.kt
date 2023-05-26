a5888fbd37fa28584cffb58eee1c5959e56080bd
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-05-26 16:06:01 -0400

    Nit: make functions inline

--- common/src/commonMain/kotlin/io/github/opletter/courseevals/common/data/Utils.kt
+++ common/src/commonMain/kotlin/io/github/opletter/courseevals/common/data/Utils.kt
@@ -12,10 +12,10 @@ fun String.substringAfterBefore(after: String, before: String): String =
 fun Double.roundToDecimal(dec: Int): Double = (this * 10.0.pow(dec)).roundToInt() / 10.0.pow(dec)
 
 //Stolen from te interwebs
-suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> =
+suspend inline fun <A, B> Iterable<A>.pmap(crossinline f: suspend (A) -> B): List<B> =
     coroutineScope { map { async { f(it) } }.awaitAll() }
 
-suspend fun <A, B, C> Map<A, B>.pmap(f: suspend (Map.Entry<A, B>) -> C): List<C> =
+suspend inline fun <A, B, C> Map<A, B>.pmap(crossinline f: suspend (Map.Entry<A, B>) -> C): List<C> =
     coroutineScope { map { async { f(it) } }.awaitAll() }
 
 fun <T> Collection<T>.prepend(element: T): List<T> = listOf(element) + this

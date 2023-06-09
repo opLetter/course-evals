c0208908c4133f64d891d76a9c26319263f2c770
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-06-09 13:10:07 -0400

    Upgrade kobweb to 0.13.5 and ktor to 2.3.1

--- site-core/src/jsMain/kotlin/io/github/opletter/courseevals/site/core/Theme.kt
+++ site-core/src/jsMain/kotlin/io/github/opletter/courseevals/site/core/Theme.kt
@@ -10,7 +10,6 @@ import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.init.registerBaseStyle
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import com.varabyte.kobweb.silk.theme.colors.MutableSilkPalette
-import com.varabyte.kobweb.silk.theme.colors.MutableSilkPalettes
 import kotlinx.browser.localStorage
 
 private const val COLOR_MODE_KEY = "course-evals:colorMode"
@@ -30,40 +29,36 @@ fun updateTheme(ctx: InitSilkContext) {
     // maybe: https://coolors.co/2b2d42-647890-8d99ae-dce9fa-edf2f4-f7cad0-ef233c-d90429
     val lightButtonBase = Color.rgb(0xD90429)
     val darkButtonBase = Color.rgb(0xEF233C)
-    ctx.theme.palettes = MutableSilkPalettes(
-        light = MutableSilkPalette(
-            background = Color.rgb(0xEDF2F4),
-            color = Colors.Black, // Color.rgb(0x2B2D42),
-            button = MutableSilkPalette.Button(
-                default = lightButtonBase,
-                hover = lightButtonBase.lightened(byPercent = 0.2f),
-                focus = Colors.CornflowerBlue,
-                pressed = lightButtonBase.lightened(byPercent = 0.3f),
-            ),
-            link = MutableSilkPalette.Link(
-                default = Colors.Blue,
-                visited = Color.rgb(123, 0, 21),
-            ),
-            tab = ctx.theme.palettes.light.tab,
-            border = Color.rgb(76, 76, 187),
-        ),
-        dark = MutableSilkPalette(
-            background = Color.rgb(0xEDF2F4), // Color.rgb(0x2B2D42)
-            color = Colors.Black,
-            button = MutableSilkPalette.Button(
-                default = darkButtonBase,
-                hover = darkButtonBase.darkened(byPercent = 0.2f),
-                focus = Colors.LightSkyBlue,
-                pressed = darkButtonBase.darkened(byPercent = 0.3f),
-            ),
-            link = MutableSilkPalette.Link(
-                default = Colors.Blue,
-                visited = Color.rgb(217, 4, 41), // Color.rgb(123, 0, 21),
-            ),
-            tab = ctx.theme.palettes.dark.tab,
-            border = Colors.Red.darkened(0.15f),
+    ctx.theme.palettes.light.apply {
+        background = Color.rgb(0xEDF2F4)
+        color = Colors.Black // Color.rgb(0x2B2D42),
+        button = MutableSilkPalette.Button(
+            default = lightButtonBase,
+            hover = lightButtonBase.lightened(byPercent = 0.2f),
+            focus = Colors.CornflowerBlue,
+            pressed = lightButtonBase.lightened(byPercent = 0.3f),
         )
-    )
+        link = MutableSilkPalette.Link(
+            default = Colors.Blue,
+            visited = Color.rgb(123, 0, 21),
+        )
+        border = Color.rgb(76, 76, 187)
+    }
+    ctx.theme.palettes.dark.apply {
+        background = Color.rgb(0xEDF2F4) // Color.rgb(0x2B2D42)
+        color = Colors.Black
+        button = MutableSilkPalette.Button(
+            default = darkButtonBase,
+            hover = darkButtonBase.darkened(byPercent = 0.2f),
+            focus = Colors.LightSkyBlue,
+            pressed = darkButtonBase.darkened(byPercent = 0.3f),
+        )
+        link = MutableSilkPalette.Link(
+            default = Colors.Blue,
+            visited = Color.rgb(217, 4, 41), // Color.rgb(123, 0, 21),
+        )
+        border = Colors.Red.darkened(0.15f)
+    }
 }
 
 class SitePalette(

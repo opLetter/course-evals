8d24f27971a89a3141791e5694f7fad65c944d5b
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-08-31 01:33:27 -0400

    Attempt to move gradle enterprise config to workflow I have a feeling this won't work

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,19 +6,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("com.gradle.enterprise") version ("3.14.1")
-}
-
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
-}
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "course-evals"

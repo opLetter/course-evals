3a901a89922dd305f12705944d8106b30161b97b
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-09-06 00:43:02 -0400

    Improve course buttons overflow particularly on desktop

--- site-core/src/jsMain/kotlin/io/github/opletter/courseevals/site/core/components/sections/dataPage/ProfSummary.kt
+++ site-core/src/jsMain/kotlin/io/github/opletter/courseevals/site/core/components/sections/dataPage/ProfSummary.kt
@@ -3,6 +3,7 @@ package io.github.opletter.courseevals.site.core.components.sections.dataPage
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.AlignSelf
 import com.varabyte.kobweb.compose.css.Overflow
+import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
@@ -13,7 +14,6 @@ import io.github.opletter.courseevals.site.core.components.widgets.PillButton
 import io.github.opletter.courseevals.site.core.states.ProfSummaryVM
 import io.github.opletter.courseevals.site.core.states.Questions
 import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.unaryMinus
 
 @Composable
@@ -36,24 +36,24 @@ fun CourseButtonsBar(
     modifier: Modifier = Modifier,
     onClick: (Int) -> Unit,
 ) {
-    val mobileScrollbarOffset = 0.5.cssRem // fixes issue on mobile where scrollbar appears too high
-    Row(
+    val scrollbarSpacing = 0.5.cssRem // leave some space between buttons & scrollbar
+    Box( // outside scroll container
         Modifier
-            .maxWidth(100.percent)
-            .flexShrink(0)
             .alignSelf(AlignSelf.Start)
-            .padding(bottom = mobileScrollbarOffset)
-            .margin(bottom = -mobileScrollbarOffset)
+            .flexShrink(0) // prevents vertical shrinking (happens otherwise on mobile)
+            .padding(bottom = scrollbarSpacing)
+            .margin(bottom = -scrollbarSpacing)
             .overflow { x(Overflow.Auto) }
-            .columnGap(0.5.cssRem)
             .fontSize(1.3.cssRem)
-            .then(modifier),
+            .then(modifier)
     ) {
-        courses.toList().forEachIndexed { index, (course, teaching) ->
-            PillButton(selected = index == selectedCourse, onClick = { onClick(index) }) {
-                Row(Modifier.columnGap(0.5.cssRem), verticalAlignment = Alignment.CenterVertically) {
-                    SpanText(course)
-                    if (teaching) FaChalkboardUser(Modifier.title("Teaching this course in Fall 2023"))
+        Row(Modifier.columnGap(0.5.cssRem)) {
+            courses.toList().forEachIndexed { index, (course, teaching) ->
+                PillButton(selected = index == selectedCourse, onClick = { onClick(index) }) {
+                    Row(Modifier.columnGap(0.5.cssRem), verticalAlignment = Alignment.CenterVertically) {
+                        SpanText(course)
+                        if (teaching) FaChalkboardUser(Modifier.title("Teaching this course in Fall 2023"))
+                    }
                 }
             }
         }
